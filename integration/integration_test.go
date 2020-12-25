68c1c8e54b4c406d094e219dff4e36dab5e94828
steevehook <steevehook@gmail.com>
2020-12-25 12:57:57 +0200

    add generic integration tests

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -30,57 +30,68 @@ const (
 )
 
 var (
-	testTime, _   = time.Parse(time.RFC3339, testTimeStr)
-	s1 = models.Stream{
-		ID: "s1-id",
-		Name: "s1-name",
+	testTime, _ = time.Parse(time.RFC3339, testTimeStr)
+	s1          = models.Stream{
+		ID:        "s1-id",
+		Name:      "s1-name",
 		CreatedAt: testTime,
 	}
 	s2 = models.Stream{
-		ID: "s2-id",
-		Name: "s2-name",
+		ID:        "s2-id",
+		Name:      "s2-name",
 		CreatedAt: testTime,
 	}
 	s3 = models.Stream{
-		ID: "s3-id",
-		Name: "s3-name",
+		ID:        "s3-id",
+		Name:      "s3-name",
 		CreatedAt: testTime,
 	}
 	s1evt1 = models.Event{
-		ID: "evt1-id",
-		StreamID: s1.ID,
-		Status: 0,
+		ID:        "evt1-id",
+		StreamID:  s1.ID,
+		Status:    0,
 		CreatedAt: testTime,
-		Body: []byte(`{"f1": "v1"}`),
+		Body:      []byte(`{"f1": "v1"}`),
 	}
 	s1evt2 = models.Event{
-		ID: "evt2-id",
-		StreamID: s1.ID,
-		Status: 0,
+		ID:        "evt2-id",
+		StreamID:  s1.ID,
+		Status:    0,
 		CreatedAt: testTime,
-		Body: []byte(`{"f2": "v2"}`),
+		Body:      []byte(`{"f2": "v2"}`),
 	}
 	s1evt3 = models.Event{
-		ID: "evt3-id",
-		StreamID: s1.ID,
-		Status: 1,
+		ID:        "evt3-id",
+		StreamID:  s1.ID,
+		Status:    1,
 		CreatedAt: testTime,
-		Body: []byte(`{"f3": "v3"}`),
+		Body:      []byte(`{"f3": "v3"}`),
 	}
 	s1evt4 = models.Event{
-		ID: "evt4-id",
-		StreamID: s1.ID,
-		Status: 2,
+		ID:        "evt4-id",
+		StreamID:  s1.ID,
+		Status:    2,
 		CreatedAt: testTime,
-		Body: []byte(`{"f4": "v4"}`),
+		Body:      []byte(`{"f4": "v4"}`),
 	}
 	s2evt1 = models.Event{
-		ID: "evt1-id",
-		StreamID: s2.ID,
-		Status: 0,
+		ID:        "evt1-id",
+		StreamID:  s2.ID,
+		Status:    0,
 		CreatedAt: testTime,
-		Body: []byte(`{"f1": "v1"}`),
+		Body:      []byte(`{"f1": "v1"}`),
 	}
+	operations = []string{
+		"create_stream",
+		"delete_stream",
+		"get_stream_info",
+		"get_stream_events",
+		"write_event",
+		"mark_event",
+		"process_events",
+		"retry_events",
+	}
+	opNotFoundReason = "operation must be one of: 'health', 'create_stream', 'delete_stream', 'get_stream_info', 'get_stream_events', 'write_event', 'process_events', 'retry_events', 'retry_events', 'mark_event', 'exit'"
 )
 
 type JSON = map[string]interface{}
@@ -166,15 +177,199 @@ func (s *appSuite) Test_Exit() {
 }
 
 func (s *appSuite) Test_Operations_DecodeRequestError() {
+	conn := s.newConn()
+	ops := append(operations, "health", "exit")
+	for _, op := range ops {
+		s.Run(op, func() {
+			_, err := conn.Write([]byte("{}\n"))
+			s.Require().NoError(err)
+
+			var res response
+			s.read(conn, &res)
+			s.False(res.Status)
+			s.Equal("decode_request", res.Operation)
+			s.Equal(opNotFoundReason, res.Reason)
+			s.Empty(res.Body)
+			s.Empty(res.Context)
+		})
+	}
 }
 
 func (s *appSuite) Test_OperationNotFound() {
+	conn := s.newConn()
+
+	s.write(conn, "operation_does_not_exist", "")
+
+	var res response
+	s.read(conn, &res)
+	s.False(res.Status)
+	s.Equal("decode_request", res.Operation)
+	s.Equal(opNotFoundReason, res.Reason)
+	s.Empty(res.Body)
 }
 
 func (s *appSuite) Test_Operations_UnAuthorizedError() {
+	s.auth = ""
+	conn := s.newConn()
+	for _, op := range operations {
+		s.Run(op, func() {
+			s.write(conn, op, "")
+
+			var res response
+			s.read(conn, &res)
+			s.False(res.Status)
+			s.Equal(op, res.Operation)
+			s.Equal("unauthorized to make request", res.Reason)
+			s.Empty(res.Body)
+			s.Empty(res.Context)
+		})
+	}
 }
 
-func (s *appSuite) Test_Operations_ParseRequestBodyError() {
+func (s *appSuite) Test_Operations_ParseRequestBody_MissingFieldsError() {
+	requests := []struct {
+		operation string
+		context   map[string]interface{}
+	}{
+		{
+			operation: "create_stream",
+			context: JSON{
+				"body": []interface{}{
+					JSON{
+						"name":     "stream_name",
+						"type":     "string",
+						"required": true,
+					},
+				},
+			},
+		},
+		{
+			operation: "delete_stream",
+			context: JSON{
+				"body": []interface{}{
+					JSON{
+						"name":     "stream_name",
+						"type":     "string",
+						"required": true,
+					},
+				},
+			},
+		},
+		{
+			operation: "get_stream_info",
+			context: JSON{
+				"body": []interface{}{
+					JSON{
+						"name":     "stream_name",
+						"type":     "string",
+						"required": true,
+					},
+				},
+			},
+		},
+		{
+			operation: "get_stream_events",
+			context: JSON{
+				"body": []interface{}{
+					JSON{
+						"name":     "stream_name",
+						"type":     "string",
+						"required": true,
+					},
+				},
+			},
+		},
+		{
+			operation: "write_event",
+			context: JSON{
+				"body": []interface{}{
+					JSON{
+						"name":     "stream_name",
+						"type":     "string",
+						"required": true,
+					},
+					JSON{
+						"name":     "event",
+						"type":     "[]byte",
+						"required": true,
+					},
+				},
+			},
+		},
+		{
+			operation: "mark_event",
+			context: JSON{
+				"body": []interface{}{
+					JSON{
+						"name":     "event_id",
+						"type":     "string",
+						"required": true,
+					},
+					JSON{
+						"name":     "status",
+						"type":     "uint8",
+						"required": true,
+					},
+				},
+			},
+		},
+		{
+			operation: "process_events",
+			context: JSON{
+				"body": []interface{}{
+					JSON{
+						"name":     "stream_name",
+						"type":     "string",
+						"required": true,
+					},
+				},
+			},
+		},
+		{
+			operation: "retry_events",
+			context: JSON{
+				"body": []interface{}{
+					JSON{
+						"name":     "stream_name",
+						"type":     "string",
+						"required": true,
+					},
+				},
+			},
+		},
+	}
+	conn := s.newConn()
+	for _, req := range requests {
+		s.Run(req.operation, func() {
+			s.write(conn, req.operation, "")
+
+			var res response
+			s.read(conn, &res)
+			s.False(res.Status)
+			s.Equal(req.operation, res.Operation)
+			s.Equal("missing required fields", res.Reason)
+			s.Equal(req.context, res.Context)
+			s.Empty(res.Body)
+		})
+	}
+}
+
+func (s *appSuite) Test_Operations_ParseRequestBody_DecodeJSONError() {
+	s.auth = ""
+	conn := s.newConn()
+	for _, op := range operations {
+		s.Run(op, func() {
+			s.write(conn, op, "{")
+
+			var res response
+			s.read(conn, &res)
+			s.False(res.Status)
+			s.Equal("decode_request", res.Operation)
+			s.Equal("invalid json provided", res.Reason)
+			s.Empty(res.Context)
+			s.Empty(res.Body)
+		})
+	}
 }
 
 func (s *appSuite) waitForServer() {
